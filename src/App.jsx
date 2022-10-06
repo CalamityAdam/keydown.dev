1aaed6bd6cda802a2c8a02029aaa81778a29d359
Adam Sisk <adamsisk91@gmail.com>
2022-10-05 20:36:17 -0500

    initial branding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import siteLogo from './assets/site-logo.svg';
+import iconLinkedin from './assets/icon-linkedin.svg';
+import iconGithub from './assets/icon-github.svg';
+import iconEmail from './assets/icon-email.svg';
 
 const initialState = {
   charCode: 0,
@@ -8,6 +12,7 @@ const initialState = {
 };
 
 function App() {
+  const year = useMemo(() => new Date().getFullYear(), []);
   const [key, setKey] = useState(initialState);
 
   useEffect(() => {
@@ -29,7 +34,17 @@ function App() {
   }, []);
 
   return (
-    <div className='h-screen bg-black text-white'>
+    <div className='flex flex-col h-screen px-6 sm:px-14 max-w-7xl mx-auto'>
+      <nav className='h-14 sm:h-24 flex items-center'>
+        <a href='https://adamsisk.info'>
+          <img
+            src={siteLogo}
+            className='sm:h-14'
+            height='56'
+            alt='A S site logo'
+          />
+        </a>
+      </nav>
       <div className='flex flex-col items-center justify-center h-full'>
         <div className='text-2xl font-bold'>Key Down</div>
         <div className='text-xl font-bold'>charCode: {key.charCode}</div>
@@ -37,6 +52,52 @@ function App() {
         <div className='text-xl font-bold'>key: {key.key}</div>
         <div className='text-xl font-bold'>keyCode: {key.keyCode}</div>
       </div>
+      <footer className='h-24 sm:h-36 lg:h-52 flex justify-end'>
+        <div className='flex flex-col justify-center items-end h-full sm:gap-2 lg:gap-4'>
+          <p className='block uppercase'>© Adam Sisk {year}</p>
+          <div className='flex gap-4'>
+            <a
+              href='https://www.linkedin.com/in/adamsisk/'
+              aria-label='linkedin'
+              title='visit me on linkedin'
+            >
+              <img
+                width='24'
+                height='24'
+                src={iconLinkedin}
+                className='icon linkedin'
+                alt='linkedin'
+              />
+            </a>
+            <a
+              href='https://github.com/calamityadam'
+              aria-label='github'
+              title='visit me on github'
+            >
+              <img
+                width='24'
+                height='24'
+                src={iconGithub}
+                className='icon github'
+                alt='github'
+              />
+            </a>
+            <a
+              href='mailto:sisk@hey.com?subject=Hi%20Adam!'
+              aria-label='email'
+              title='send me an email'
+            >
+              <img
+                width='24'
+                height='24'
+                src={iconEmail}
+                className='icon email'
+                alt='email'
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
